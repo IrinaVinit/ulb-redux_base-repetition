6fbfd8903f9d3d969f280b9a6d0cbca4254e8eb0
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-08-30 14:58:13 +0300

    init: add cashReduser and customerReduser

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from "redux";
+import { cashReducer } from "./cashReduser";
+import { customerReducer } from "./customerReduser";
+
+const rootReducer = combineReducers(
+    {
+        cash: cashReducer,
+        customers: customerReducer
+    }
+)
+
+export const store = createStore(rootReducer);
